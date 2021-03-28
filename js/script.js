874f93f57200df11a0f1ede63df9a3f7e58928d2
Juli Nakai <julidev.tk@gmail.com>
2021-03-28 16:12:50 -0300

    Javascript2

--- js/script.js
+++ js/script.js
@@ -11,21 +11,24 @@
   //time: 5000, // tempo de transição dos slides
   //pauseOnHover: true, // pausa a transição automática});
 
-if(window.SimpleSlide){
+
+if(window.SimpleSlide){ // só vai rodar se o SimpleSlide existir
+  new SimpleSlide({
+  slide: "quote",     // nome do atributo data-slide="principal"
+  auto: true,             // se o slide deve passar automaticamente/ true é padrão... ou false
+  time: 5000,             // tempo de transição dos slides de 5 segundos
+  pauseOnHover: true,     // pausa a transição automática});
+  })
+
+  new SimpleSlide({
+  slide: "portfolio", // nome do atributo data-slide="principal"
+  nav: true, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides;
+  })
+  }
 
 
-new SimpleSlide({
-slide: "quote",     // nome do atributo data-slide="principal"
-auto: true,             // se o slide deve passar automaticamente/ true é padrão... ou false
-time: 5000,             // tempo de transição dos slides de 5 segundos
-pauseOnHover: true,     // pausa a transição automática});
-})
-
-new SimpleSlide({
-slide: "portfolio", // nome do atributo data-slide="principal"
-nav: true, // se deve ou não mostrar a navegação
-auto: true, // se o slide deve passar automaticamente
-time: 5000, // tempo de transição dos slides;
-})
-
+if(window.SimpleAnime){ // só vai rodar se o SimpleAnime
+  new SimpleAnime();
 }
